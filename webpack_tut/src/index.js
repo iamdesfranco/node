05b8c9c4534e8c0007a589733ab4a8c3849559a0
Franco <git@francolabuschagne.com>
2019-05-29 14:33:56 +0200

    Production Webpack

--- webpack_tut/src/index.js
+++ webpack_tut/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './style.scss'
 
-const Greeting = () => "Hello"
+const Greeting = () => <h1>Hello World</h1>
 
 render (
     <Greeting/>,
--- webpack_tut/webpack.config.js
+++ webpack_tut/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -16,8 +17,25 @@ module.exports = {
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react']
                     }
-                },
-            }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    // fallback to style-loader in development
+                    process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
         ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 }
